fea4f27477a910565a975b1c28300b9941116723
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-08-05 09:24:31 +0300

    Доработка модального окна: вариативность контроллеров

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -9,20 +9,49 @@ export function animationOpen(modal, pageWrapper) {
     const description = target.querySelector('.Panel-Description').textContent;
     modal.querySelector('.Modal-Title').innerHTML = `<b>${title}</b>`;
     modal.querySelector('.Modal-Description').innerHTML = description;
+
+    const modalIcon = modal.querySelector('.Modal-Icon');
+    const { iconType, controller, currentValue } = target.dataset;
+    modalIcon.src = `../assets/icon_${iconType}_active.svg`;
+    modalIcon.setAttribute('alt', `icon-${iconType}`);
+
+    let valueElement = modal.querySelector('.Modal-Value');
+    if (iconType === 'temperature') {
+      let setValue = currentValue;
+      if (currentValue > 0) setValue = `+${currentValue}`;
+      valueElement.innerHTML = `<b>${setValue}</b>`;
+    } else {
+      valueElement.innerHTML = '';
+    }
+
+    let controllerElement;
+    if (controller === 'range' && iconType === 'sun') {
+      controllerElement = modal.querySelector('.Modal-LightController');
+    } else if (controller === 'range' && iconType === 'temperature') {
+      controllerElement = modal.querySelector('.Modal-TemperatureController');
+    } else if (controller === 'rotation') {
+      controllerElement = modal.querySelector('.Modal-FloorController');
+    } else {
+      console.warn('[Error] in Modal.animationOpen method: not find correct controller');
+    }
+
+    controllerElement.classList.remove('Modal_hidden');
+    if (controller === 'range')
+      controllerElement.querySelector('input[type="range"]').setAttribute('value', currentValue);
+
     modal.classList.remove('Modal_hidden');
     pageWrapper.classList.add('Modal_blure');
     document.querySelector('body').classList.add('Modal_body');
     modal.style.top = `${pageYOffset}px`;
-    const modalIcon = modal.querySelector('.Modal-Icon');
-    const { iconType } = target.dataset;
-    modalIcon.src = `../assets/icon_${iconType}_active.svg`;
-    modalIcon.setAttribute('alt', `icon-${iconType}`);
   };
 }
 
 export function animationClose(modal, pageWrapper) {
   return event => {
     modal.classList.add('Modal_hidden');
+    let controllers = modal.querySelector('.Modal-Controller').children;
+    controllers = Array.prototype.slice.call(controllers);
+    controllers.forEach(element => element.classList.add('Modal_hidden'));
     pageWrapper.classList.remove('Modal_blure');
     document.querySelector('body').classList.remove('Modal_body');
     modal.style.top = '';
